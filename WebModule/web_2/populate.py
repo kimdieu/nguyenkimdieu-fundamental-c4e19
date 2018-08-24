229d912a4505617fc3f8e740bb12eff79fb5701c
Dieu Kim Nguyen <dieukimnguyen@Dieus-MacBook-Air.local>
2018-08-24 19:56:07 +0700

    final

--- WebModule/web_2/populate.py
+++ WebModule/web_2/populate.py
@@ -11,7 +11,9 @@ image_list = ['https://kenh14cdn.com/2017/1475129038474401-1485915456325.jpg',
               'http://static.ilike.com.vn:8880/uploads/2018/07/aec223b9gy1ftbwiusstij21ev1evtxh-1532686944437811952872.jpg',
               'https://znews-photo-td.zadn.vn/w1024/Uploaded/kcwvouvs/2017_09_15/20729587_877826279043507_3679078010070318122_n.jpg',
               'http://media.tinmoi.vn/2013/10/21/nu-sinh-duoc-ton-vinh-nu-than-1.jpg',
-              'https://anh.eva.vn//upload/1-2016/images/2016-03-22/1622059_1671230623141576_7013179149578667299_n-1458611822-width960height960.jpg']
+              'https://anh.eva.vn//upload/1-2016/images/2016-03-22/1622059_1671230623141576_7013179149578667299_n-1458611822-width960height960.jpg',
+              '../static/image/hotgirl.jpg',
+              '../static/image/hotbao.jpg']
 
 for image_link in image_list:
 
